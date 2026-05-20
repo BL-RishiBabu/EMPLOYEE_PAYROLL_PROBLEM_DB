9631f4f9c267435b35fe0a0fba953e18db761fe8
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-20 14:12:44 +0530

    Step four is completed

--- query/steps.sql
+++ query/steps.sql
@@ -20,3 +20,7 @@ INSERT INTO employee_payroll (name, salary, start) VALUES
 ('Bill', 1000000.00, '2018-01-03'),
 ('Terisa', 2000000.00, '2019-11-13'),
 ('Charlie', 3000000.00, '2020-05-21');
+
+-- Step 4: Retrieve all employee payroll data from the table (CRUD - Read Operation)
+
+SELECT * FROM employee_payroll;
