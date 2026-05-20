8c4e1ca83a1e4aa9dfd7a600df2f8782264c695d
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-20 12:41:54 +0530

    altered

--- query/stepTwo.sql
+++
@@ -1,9 +0,0 @@
-use payroll_services;
-
-create table employee_payroll(
-    id int not null auto_increment,
-    name varchar(50) not null,
-    salary decimal(10,2) not null,
-    start date not null,
-    primary key(id)
-);
